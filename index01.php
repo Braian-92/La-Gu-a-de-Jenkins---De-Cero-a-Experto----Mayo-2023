e4bbaac45b80fa20a22b568a43f7cead26a8d056
Braian-92 <zamudiobraianhernan@gmail.com>
2023-05-29 20:40:52 -0300

    configuraciones para conectarse con ssh y putty

---
+++ index01.php
@@ -0,0 +1,12 @@
+https://www.osboxes.org/ubuntu/#ubuntu-22-10-vmware
+
+la clave del sistema es la misma que el usuario
+
+//! actualizar sistema = sudo apt-get update
+clear = limpiar pantalla
+sudo apt-get install vim = instalar editor vin
+sudo apt install openssh-server = instalar openssh
+sudo systemctl status ssh = verficar que ande bien (tiene que salir en verde activo)
+ip a = verificar la ip de la pc
+sudo apt-get install net-tools
+ifconfig = para traer la IP iner para la conexion ssh (https://www.youtube.com/watch?v=YNKPem3aDPA)
