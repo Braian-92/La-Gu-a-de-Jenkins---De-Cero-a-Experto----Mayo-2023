62905167b3aa7dc9361b1449f547f51a65176c32
Braian-92 <zamudiobraianhernan@gmail.com>
2023-05-29 21:52:59 -0300

    pasos base para instalar docker

--- index01.php
+++ index01.php
@@ -9,4 +9,12 @@ sudo apt install openssh-server = instalar openssh
 sudo systemctl status ssh = verficar que ande bien (tiene que salir en verde activo)
 ip a = verificar la ip de la pc
 sudo apt-get install net-tools
-ifconfig = para traer la IP iner para la conexion ssh (https://www.youtube.com/watch?v=YNKPem3aDPA)
+ifconfig = para traer la IP innet para la conexion ssh (https://www.youtube.com/watch?v=YNKPem3aDPA)
+(192.168.1.68 en mi caso de ip local)
+ingresar por putty con el uo innet y la clave del sistema operativo
+. En la sección "Host Name (or IP address)", introduce osboxes@<IP_UBUNTU> (reemplaza <IP_UBUNTU> con la dirección IP de tu máquina virtual Ubuntu).
+
+instalar cmd windows = https://cmder.app/
+
+
+instalar docker en ubuntu = https://docs.docker.com/engine/install/ubuntu/
