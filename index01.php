a4e676355cc45c0b0305ee1211739ba22b40a3d8
Braian-92 <zamudiobraianhernan@gmail.com>
2023-05-29 23:32:08 -0300

    docker funcional

--- index01.php
+++ index01.php
@@ -18,3 +18,29 @@ instalar cmd windows = https://cmder.app/
 
 
 instalar docker en ubuntu = https://docs.docker.com/engine/install/ubuntu/
+
+paso 1
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+paso 2
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+parte 3 (no se visualiza nada al ejecutarlo)
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl start docker (luego pide la contraseña)
+
+sudo usermod -aG docker osboxes (poner el usuario osboxes en este caso)
+
+reiniciar linux
+docker ps (para verificar buen funcionamiento)
