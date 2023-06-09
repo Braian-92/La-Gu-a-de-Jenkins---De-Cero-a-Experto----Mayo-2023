4931df3caf2820e2316c28f09e07a7fc40e0100c
Braian-92 <zamudiobraianhernan@gmail.com>
2023-06-09 02:27:51 -0300

    DSL parámetros, crones, ejecuciones y publicaciones

--- index01.php
+++ index01.php
@@ -769,3 +769,97 @@ job('ejemplo-job-DSL') {
     }
   }
 }
+
+------------
+
+job('ejemplo-job-DSL') {
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('macloujulian')
+      node / gitConfigEmail('macloujulian@gmail.com')
+    }
+  }
+  parameters {
+  	stringParam('nombre', defaultValue = 'Braian', description = 'parametro de cadena para el job booleano')
+  	choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+  	booleanParam('Agente', false)
+  }
+}
+
+########## disparadores de ejecución CRON #########
+
+job('ejemplo-job-DSL') {
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('macloujulian')
+      node / gitConfigEmail('macloujulian@gmail.com')
+    }
+  }
+  parameters {
+  	stringParam('nombre', defaultValue = 'Braian', description = 'parametro de cadena para el job booleano')
+  	choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+  	booleanParam('Agente', false)
+  }
+  triggers {
+  	cron('H/7 * * * *')
+  }
+}
+
+########### STEPS (EJECUTAR) ########
+
+job('ejemplo-job-DSL') {
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('macloujulian')
+      node / gitConfigEmail('macloujulian@gmail.com')
+    }
+  }
+  parameters {
+  	stringParam('nombre', defaultValue = 'Braian', description = 'parametro de cadena para el job booleano')
+  	choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+  	booleanParam('Agente', false)
+  }
+  triggers {
+  	cron('H/7 * * * *')
+  }
+  steps {
+  	shell("bash jobscript.sh")
+  }
+}
+
+########## PUBLISHERS ###########
+RECORDAR QUE TODA LA DOCUMENTACIÓN SE ENCUENTRA EN.... ("https://jenkinsci.github.io/job-dsl-plugin/#method/javaposse.jobdsl.dsl.helpers.ScmContext.git")
+RECORDAR QUE TODA LA DOCUMENTACIÓN SE ENCUENTRA EN.... ("https://jenkinsci.github.io/job-dsl-plugin/#method/javaposse.jobdsl.dsl.helpers.ScmContext.git")
+RECORDAR QUE TODA LA DOCUMENTACIÓN SE ENCUENTRA EN.... ("https://jenkinsci.github.io/job-dsl-plugin/#method/javaposse.jobdsl.dsl.helpers.ScmContext.git")
+RECORDAR QUE TODA LA DOCUMENTACIÓN SE ENCUENTRA EN.... ("https://jenkinsci.github.io/job-dsl-plugin/#method/javaposse.jobdsl.dsl.helpers.ScmContext.git")
+
+ejemplo --> activar Artifacts en Maven
+
+ejemplo --> activar mailer
+
+job('ejemplo-job-DSL') {
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('macloujulian')
+      node / gitConfigEmail('macloujulian@gmail.com')
+    }
+  }
+  parameters {
+  	stringParam('nombre', defaultValue = 'Braian', description = 'parametro de cadena para el job booleano')
+  	choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+  	booleanParam('Agente', false)
+  }
+  triggers {
+  	cron('H/7 * * * *')
+  }
+  steps {
+  	shell("bash jobscript.sh")
+  }
+  publishers {
+  	mailer('zamudiobraianhernan@gmail.com', true, true)
+  }
+}
